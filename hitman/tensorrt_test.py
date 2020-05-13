98f875ac1063ee13b0abe781155fba71315e3950
Francisco Perez-Sorrosal <fperezsorrosal@gmail.com>
2020-05-13 01:03:20 -0700

    Add tensorrt simple grpc simple client for onnx, torchscript

--- hitman/tensorrt_test.py
+++ hitman/tensorrt_test.py
@@ -1,12 +1,26 @@
+import argparse
+
 import numpy as np
 from tensorrtserver.api import *
 
 if __name__ == '__main__':
 
-    batch_size = 1
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch-size', type=int, default=1,
+                        help='batch size for inference. default: 1')
+    parser.add_argument("--server-url", type=str, default="localhost:9001",
+                        help="Inference server URL. Default is localhost:9001.")
+    parser.add_argument('--format', type=str, required=False, default='onnx',
+                        help='Format ("ts"/"onnx") used to ' +
+                             'communicate with inference service. Default is "onnx".')
+    args = parser.parse_args()
+
+    print("Format: {}".format(args.format))
+
+    batch_size = args.batch_size
     protocol = ProtocolType.from_str('grpc')
 
-    health_ctx = ServerHealthContext("localhost:9001", protocol, http_headers="", verbose=True)
+    health_ctx = ServerHealthContext(args.server_url, protocol, http_headers="", verbose=True)
     print(health_ctx.is_live())
 
     input_ids = np.random.randint(low=0, high=10000, size=512)
@@ -24,38 +38,49 @@ if __name__ == '__main__':
     print(segment_ids.shape)
 
     # prepare inputs
-    input_dict = {
-        "input__0": tuple(input_ids[i] for i in range(batch_size)),
-        "input__1": tuple(input_mask[i] for i in range(batch_size)),
-        "input__2": tuple(segment_ids[i] for i in range(batch_size))
-    }
+    if args.format == "ts":
+        input_dict = {
+            # "input__0": tuple(input_ids[i] for i in range(batch_size)),
+            # "input__1": tuple(input_mask[i] for i in range(batch_size)),
+            # "input__2": tuple(segment_ids[i] for i in range(batch_size))
+            "input__0": [input_ids],
+            "input__1": [input_mask],
+            "input__2": [segment_ids]
+        }
+    else:
+        input_dict = {
+            "input_ids": [input_ids],
+            "input_mask": [input_mask],
+            "token_type_ids": [segment_ids]
+        }
 
-    input_dict = {
-        "input_ids": [input_ids],
-        "input_mask": [input_mask],
-        "token_type_ids": [segment_ids]
-    }
-
-    print(input_dict["token_type_ids"])
 
     # prepare outputs
-    output_keys = [
-        "output",
-        # "output__1"
-    ]
+
+    if args.format == "ts":
+        output_keys = [
+            "output__0",
+        ]
+    else:
+        output_keys = [
+            "output",
+        ]
 
     output_dict = {}
     for k in output_keys:
         output_dict[k] = InferContext.ResultFormat.RAW
 
-    infer_ctx = InferContext("localhost:9001", protocol, 'oic', -1, "", True)
+    infer_ctx = InferContext(args.server_url, protocol, 'oic', -1, "", True)
 
     result = infer_ctx.run(input_dict, output_dict, batch_size)
 
     # get the result
-    print(len(result["output"]))
-    start_logits = result["output"][0].tolist()
-    # end_logits = result["output__1"][0].tolist()
+    if args.format == "ts":
+        print(len(result["output__0"]))
+        start_logits = result["output__0"][0].tolist()
+    else:
+        print(len(result["output"]))
+        start_logits = result["output"][0].tolist()
 
     print(start_logits)
     # print(end_logits)
